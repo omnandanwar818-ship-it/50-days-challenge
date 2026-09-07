fef846ccfc8b5ce4577ab3edd4ad6e93a01e68a1
omnandanwar818-ship-it <omnandanwar818@gmail.com>
2026-09-08 00:05:42 +0530

    Time: 0 ms (100%), Space: 24.4 MB (73.68%) - LeetHub

--- 2951-find-the-peaks/2951-find-the-peaks.cpp
+++ 2951-find-the-peaks/2951-find-the-peaks.cpp
@@ -1,34 +1,34 @@
-// class Solution {
-// public:
-//     vector<int> findPeaks(vector<int>& mountain) {
-//     vector<int>ans;
-//     int n=mountain.size();
-//     for(int i=1; i<n-1; i++){
-//         if((i==0 ||mountain[i]>mountain[i-1]) && (i==n-1 ||mountain[i]>=mountain[i+1])){
-//          ans.push_back(i);
-//         }
-//     }
-//     return ans;
-//         }
-// };
-#include <vector>
-
 class Solution {
 public:
-    std::vector<int> findPeaks(std::vector<int>& mountain) {
-        std::vector<int> peaks;
-        int n = mountain.size();
-        
-        // Condition 2: The first and last elements are NOT a peak.
-        // We guarantee this by starting the loop at index 1 and stopping at n - 2.
-        for (int i = 1; i < n - 1; ++i) {
-            
-            // Condition 1: An element must be STRICTLY GREATER than its neighboring elements.
-            if (mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1]) {
-                peaks.push_back(i); // Store the index of the peak
-            }
+    vector<int> findPeaks(vector<int>& mountain) {
+   std:: vector<int>ans;
+    int n=mountain.size();
+    for(int i=1; i<n-1; i++){
+        if(mountain[i]>mountain[i-1] && mountain[i]>mountain[i+1]){
+         ans.push_back(i);
         }
-        
-        return peaks;
     }
+    return ans;
+        }
 };
+// #include <vector>
+
+// class Solution {
+// public:
+//     std::vector<int> findPeaks(std::vector<int>& mountain) {
+//         std::vector<int> peaks;
+//         int n = mountain.size();
+        
+//         // Condition 2: The first and last elements are NOT a peak.
+//         // We guarantee this by starting the loop at index 1 and stopping at n - 2.
+//         for (int i = 1; i < n - 1; ++i) {
+            
+//             // Condition 1: An element must be STRICTLY GREATER than its neighboring elements.
+//             if (mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1]) {
+//                 peaks.push_back(i); // Store the index of the peak
+//             }
+//         }
+        
+//         return peaks;
+//     }
+// };
