392f5e908fbc5980dfa0308abfc7c898c3895f0a
omnandanwar818-ship-it <omnandanwar818@gmail.com>
2026-09-11 07:59:09 +0530

    Time: 0 ms (100%), Space: 8.7 MB (35.92%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        if(x==0) return true;
+      int origionalnum=x;
+      long long sum=0;
+      while(x>0){
+        sum=(sum*10)+(x%10);
+        x/=10;
+      }
+      return origionalnum==sum;
+    }
+};
