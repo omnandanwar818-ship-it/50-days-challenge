48866a17f559fc6d464d253884ef09574b1fa9ad
omnandanwar818-ship-it <omnandanwar818@gmail.com>
2026-09-14 23:26:09 +0530

    Time: 0 ms (100%), Space: 11.9 MB (62.32%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int oddcount=0;
+        for(int i=0; i<arr.size(); i++){
+            if(arr[i]%2!=0){
+                oddcount++;
+                if(oddcount==3){
+                    return true;
+                }
+            }else{
+                oddcount=0;
+            }
+        }
+        return false;
+    }
+};
